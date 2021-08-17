74dc10ee1210c6c07ea21d9fca7ace0b352f32f7
hericlesLima <hericlesrecheado@gmail.com>
2021-08-17 11:12:56 -0100

    creating the views

---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Content.css'
+
+const Content = props => (
+    <main className='Content'>
+        Componente App
+    </main>
+)
+
+export default Content
---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Home.css'
+
+const Home = props => (
+    <div className= 'Home'>
+        Componente 
+    </div>
+)
+
+export default Home
