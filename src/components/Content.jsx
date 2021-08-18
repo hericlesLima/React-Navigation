609a9ac6d43111d0f35a713eabc846495ed5d90c
hericlesLima <hericlesrecheado@gmail.com>
2021-08-18 11:15:55 -0100

    add page not found

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,6 +3,8 @@ import {Switch, Route} from 'react-router-dom'
 import About from '../views/examples/About'
 import Home from '../views/examples/Home'
 import Param from '../views/examples/Param'
+import NotFound from '../views/examples/notFound'
+
 import './Content.css'
 
 const Content = props => (
@@ -11,11 +13,17 @@ const Content = props => (
             <Route exact path='/'>
                 <Home/>
             </Route>
+
             <Route path='/about'>
                 <About/>
             </Route>
+
             <Route path='/param/:id'>
                 <Param/>
+            </Route>
+
+            <Route path='*'>
+                <NotFound/>
             </Route>
         </Switch>
     </main>
---
+++ src/views/examples/notFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './notFound.css'
+
+const NotFound = props => (
+    <div className= 'NotFound'>
+        <h1>Page not found !!!</h1>
+    </div>
+)
+
+export default NotFound
